2a5c92421ff8e7e99763f912abd04a3ae16f0060
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-08-08 09:37:58 +0300

    Fixed html and css, added js for section

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
-const navLinks = document.querySelectorAll(".nav-link");
+const navLinks = document.querySelectorAll(".nav-link"),
+  mainHome = document.querySelector('#main-home'),
+  sections = document.querySelectorAll(".section"),
+  home = document.querySelector('.home')
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  if (home) {
+    home.classList.add("actived");
+  }
+
+  
+  mainHome.classList.add("active");
+});
+
 navLinks.forEach((link) => {
-  link.addEventListener("click", function (e) {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const currentActiveSection = document.querySelector(".section.actived");
+        
+        const targetId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (currentActiveSection) {
+            
+            currentActiveSection.classList.add('leaving');
+            
+            setTimeout(() => {
+                currentActiveSection.classList.remove("actived", "leaving");
+            }, 600); 
+        }
+
+        if (targetSection) {
+            
+            targetSection.classList.add("actived");
+        }
+
+        
+        navLinks.forEach((navLink) => navLink.classList.remove("active"));
+        mainHome.classList.remove("active");
+        link.classList.add("active");
+    });
+});
+
+mainHome.addEventListener("click", (e) => {
     e.preventDefault();
 
-    navLinks.forEach((link) => link.classList.remove("active"));
+    const currentActiveSection = document.querySelector(".section.actived");
+    const homeSection = document.getElementById('home');
 
-    this.classList.add("active");
-  });
+    if (currentActiveSection && currentActiveSection !== homeSection) {
+        currentActiveSection.classList.add('leaving');
+        setTimeout(() => {
+            currentActiveSection.classList.remove("actived", "leaving");
+        }, 600);
+    }
+
+    if (homeSection) {
+        homeSection.classList.add("actived");
+    }
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    mainHome.classList.add("active");
 });
+
+
+
